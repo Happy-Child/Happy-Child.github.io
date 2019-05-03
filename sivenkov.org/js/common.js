64382162e2227c19a62666cdc6f936362fa674a7
Egor <lazuka_99@mail.ru>
2019-05-03 03:36:18 +0300

    First

---
+++ sivenkov.org/js/common.js
@@ -0,0 +1,48 @@
+"use strict";
+
+$(function () {
+
+	// -- Preloader logic BEGIN
+	const preloaderLogic = () => {
+		const preloaderWrapper = $(".preloader");
+		const preloaderLogo = $('.preloader__logo-img');
+		const preloaderLogoAnimationEl = document.querySelector('.preloader__logo-img');
+		const animationDuration = 1700;
+
+		const preloaderHide = () => {
+			preloaderWrapper.addClass("preloader_hide");
+		};
+
+		const animationLogo = () => {
+			const preloaderLogoAnimation = new LazyLinePainter(preloaderLogoAnimationEl, {
+				"ease": "easeOutQuart",
+				"strokeWidth": 1,
+				"strokeOpacity": 1,
+				"strokeColor": "#fff",
+				"strokeCap": "square"
+			});
+
+			preloaderLogoAnimation.paint();
+
+			setTimeout(() => {
+		    preloaderLogo.addClass("fill-show");
+		    preloaderHide();
+		  }, animationDuration - 400);
+		};
+
+		//Initialization BEGIN
+		animationLogo();
+		//Initialization END
+	};
+	// -- Preloader logic END
+
+
+
+	// -- Initialization work functions BEGIN
+	const initializationFunctions = () => {
+		preloaderLogic();
+	};
+	initializationFunctions();
+	// -- Initialization work functions END
+
+});
