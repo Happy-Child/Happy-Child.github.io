0b8b076e1193c8303b4f9cc2a98e7dcbb194d060
Happy-Child <40471724+Happy-Child@users.noreply.github.com>
2019-05-10 10:52:33 +0300

    fix

--- sivenkov-landing/send.php
+++ sivenkov-landing/send.php
@@ -1,24 +1,22 @@
 <?php
 
-//Upload files --------------------- START
-require_once 'PHPMailer/PHPMailerAutoload.php';
+require 'PHPMailer/PHPMailerAutoload.php';
 
-$mail = new PHPMailer;
+$mail = new PHPMailer;  
 $mail->IsSMTP();
 $mail->SMTPDebug = 1;
 $mail->SMTPAuth = true;
 $mail->SMTPSecure = 'ssl';
 $mail->Host = "smtp.gmail.com";
-$mail->Port = 465; // or 587
+$mail->Port = 465; // or 587  
 $mail->IsHTML(true);
 $mail->Username = "sendingsivenkov@gmail.com";
 $mail->Password = "1Jfdks8Hns";
-$mail->setFrom('mozerqake1999@gmail.com', 'Заявка с сайта sivenkov.by');
-$mail->addAddress('mozerqake1999@gmail.com', 'Заявка с сайта sivenkov.by');
-$mail->addReplyTo('mozerqake1999@gmail.com', 'Заявка с сайта sivenkov.by');
-$mail->isHTML(true);
+$mail->setFrom('sendingsivenkov@gmail.com', 'Заявка с сайта sivenkov.by');
+$mail->addAddress('sivenkovvlad@gmail.com', 'Заявка с сайта sivenkov.by');
+$mail->addReplyTo('sivenkovvlad@gmail.com', 'Заявка с сайта sivenkov.by');
+$mail->isHTML(true); 
 $mail->Subject = 'Заявка с сайта Sivenkov.by';
-
 
 $name = $_POST["name"];
 $company = $_POST["company"];
@@ -39,3 +37,4 @@ if (!$sendMail) echo 'Mailer Error: ' . $mail->ErrorInfo;
 else echo 'Mailer SENDSENDSEND';
 
 ?>
+
