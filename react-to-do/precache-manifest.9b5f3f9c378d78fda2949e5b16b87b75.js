8765c55012f92c1620b275faad964e4294ea3546
Happy-Child <lazuka_99@mail.ru>
2020-05-05 01:26:18 +0300

    Added new project

---
+++ react-to-do/precache-manifest.9b5f3f9c378d78fda2949e5b16b87b75.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d1e08b8a46dbbcb9a764f6dc4f999172",
+    "url": "./index.html"
+  },
+  {
+    "revision": "7acd0fc9ea66c5375600",
+    "url": "./static/css/main.e9509199.chunk.css"
+  },
+  {
+    "revision": "fb8353127b4dc119548b",
+    "url": "./static/js/2.aee8a034.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.aee8a034.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7acd0fc9ea66c5375600",
+    "url": "./static/js/main.f0d969f7.chunk.js"
+  },
+  {
+    "revision": "4757db3b0015dd477e2f",
+    "url": "./static/js/runtime-main.1483024b.js"
+  },
+  {
+    "revision": "e0fea42b0e43c12f561a444c625f6be3",
+    "url": "./static/media/icon_check.e0fea42b.svg"
+  }
+]);
